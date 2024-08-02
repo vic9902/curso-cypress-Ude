1785d09a6667be7af2e3447ef9978f3a44be453b
Victor Estrada <victor.estrada@himedsolutions.com>
2024-08-02 07:45:01 -0500

    se agrega la configuración de reportes con allure

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "cypress";
+import { configureAllureAdapterPlugins } from '@mmisty/cypress-allure-adapter/plugins';
 
 export default defineConfig({
   e2e:{
     setupNodeEvents(on, config){
+      configureAllureAdapterPlugins(on, config);
+      return config;
       // implement node event listeners here
     }
   },
